47541d32d02cfb177f3c65604da886972f9b8f14
Shreya Ghorui <shreyaghorui202@gmail.com>
2026-04-28 13:35:59 +0530

    langchain fixing

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-from groq import Groq
-import os
 from dotenv import load_dotenv
+import os
+
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="llama-3.1-8b-instant",
+    temperature=0.3
+)
 
 def get_response(messages):
-
-    system_prompt = {
-        "role": "system",
-        "content": (
+    system_prompt = SystemMessage(
+        content=(
             "You are a medical assistant chatbot. "
             "Answer ONLY medicine-related questions. "
             "Give SHORT answers (1-2 lines only). "
             "No long explanation. "
             "If not medical, reply: Please ask medical questions only."
         )
-    }
-
-    response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        messages=[system_prompt] + messages,
-        max_tokens=60
     )
 
-    return response.choices[0].message.content
+    chat_messages = [system_prompt]
+
+    for msg in messages:
+        if msg["role"] == "user":
+            chat_messages.append(HumanMessage(content=msg["content"]))
+
+    
+    response = llm.invoke(chat_messages)
+
+    return response.content
